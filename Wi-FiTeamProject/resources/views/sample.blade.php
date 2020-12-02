9e4b48303428bcf5c28d4e9433405f0545ac6d39
admin <2019stu03@nagoya-vti.ac.jp>
2020-12-02 11:37:50 +0900

    201202 環境構築暫定終了バージョン

---
+++ Wi-FiTeamProject/resources/views/sample.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div id="app"> 
+    <example-component></example-component>
+    <h1>hello!</h1>
+    <p>サンプルページです。</p>
+    </div>
+    <script src=" {{ mix('js/app.js') }} "></script>
+</body>
+</html>
